4212b624ee125c09c4ca16f2556ba62ce5be51d7
Thomas Greco <thomasjosephgreco@yahoo.com>
2017-11-22 08:02:36 -0500

    pushing reformatted code w/ some starting algorithms & tests

--- js-practice/solutions.js
+++ js-practice/solutions.js
@@ -45,81 +45,85 @@ const fns = {
 };
 
 
-test('indexLookup', assert => {
-    const array = [1,2,3,4,5];
-    const msg = 'should equal array.indexOf()';
-    const actual = fns.indexOf(array, 3);
-    const expected = 2;
-    console.log(actual, 'expected =>', expected);
-    assert.same(actual, expected, msg);
-    assert.end();
-});
+const testSuite = () => {
+    test('indexLookup', assert => {
+        const array = [1,2,3,4,5];
+        const msg = 'should equal array.indexOf()';
+        const actual = fns.indexOf(array, 3);
+        const expected = 2;
+        console.log(actual, 'expected =>', expected);
+        assert.same(actual, expected, msg);
+        assert.end();
+    });
 
-test('evens fn', assert => {
-    const msg = 'should return a new list containing even numbers.';
-    const d = [1,2,3,4,5];
+    test('evens fn', assert => {
+        const msg = 'should return a new list containing even numbers.';
+        const d = [1,2,3,4,5];
 
-    const actual = fns.evens(d);
-    const expected = [2,4];
+        const actual = fns.evens(d);
+        const expected = [2,4];
 
-    assert.same(actual, expected, msg);
-    assert.end();
-});
+        assert.same(actual, expected, msg);
+        assert.end();
+    });
 
-test('evensFilter fn', assert => {
-    const msg = 'should return a new list containing even numbers.';
-    const d = [1,2,3,4,5];
+    test('evensFilter fn', assert => {
+        const msg = 'should return a new list containing even numbers.';
+        const d = [1,2,3,4,5];
 
-    const actual = fns.evensFilter(d);
-    const expected = [2,4];
+        const actual = fns.evensFilter(d);
+        const expected = [2,4];
 
-    assert.same(actual, expected, msg);
-    assert.end();
-});
+        assert.same(actual, expected, msg);
+        assert.end();
+    });
 
-test('split fn', assert => {
-    const msg = 'should split a word into an array where each letter is an item';
+    test('split fn', assert => {
+        const msg = 'should split a word into an array where each letter is an item';
 
-    const actual = fns.splitStr('Thomas');
-    const expected = ['T','h','o','m','a','s'];
+        const actual = fns.splitStr('Thomas');
+        const expected = ['T','h','o','m','a','s'];
 
-    assert.same(actual, expected, msg);
-    assert.end()
-})
+        assert.same(actual, expected, msg);
+        assert.end()
+    })
 
-test('sum fn', assert => {
-    const msg = 'should add up all items in an array';
-    
-    const arr = [0,1,2,3];
+    test('sum fn', assert => {
+        const msg = 'should add up all items in an array';
+        
+        const arr = [0,1,2,3];
 
-    const actual = fns.sum(arr);
-    const expected = 6;
+        const actual = fns.sum(arr);
+        const expected = 6;
 
-    assert.same(actual, expected, msg);
-    assert.end();
-})
+        assert.same(actual, expected, msg);
+        assert.end();
+    })
 
-test('sum loop', assert => {
-    const msg = 'should add up all items in an array';
-    
-    const arr = [0,1,2,3];
+    test('sum loop', assert => {
+        const msg = 'should add up all items in an array';
+        
+        const arr = [0,1,2,3];
 
-    const actual = fns.sumLoop(arr);
-    const expected = 6;
+        const actual = fns.sumLoop(arr);
+        const expected = 6;
 
-    assert.same(actual, expected, msg);
-    assert.end();
-})
+        assert.same(actual, expected, msg);
+        assert.end();
+    })
 
 
-test('sum loop #2', assert => {
-    const msg = 'should add up all items in an array';
-    
-    const arr = [0,1,2,3];
+    test('sum loop #2', assert => {
+        const msg = 'should add up all items in an array';
+        
+        const arr = [0,1,2,3];
 
-    const actual = fns.sumLoopTwo(arr);
-    const expected = 6;
+        const actual = fns.sumLoopTwo(arr);
+        const expected = 6;
 
-    assert.same(actual, expected, msg);
-    assert.end();
-});
+        assert.same(actual, expected, msg);
+        assert.end();
+    });
+}
+
+module.exports = {fns, testSuite};
